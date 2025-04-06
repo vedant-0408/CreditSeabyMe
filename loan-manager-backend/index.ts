070bd8e8af2806f8900d2753a8824bba897a68af
vedantsarawagi123@gmail.com <vedantsarawagi123@gmail.com>
2025-04-07 01:53:37 +0530

    improved

--- loan-manager-backend/index.ts
+++ loan-manager-backend/index.ts
@@ -11,7 +11,7 @@ import applicationRoutes from './src/routes/applications';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors());
