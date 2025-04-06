4c3dd9128c2d51c9f16a9df7034f82dd5a397229
vedantsarawagi123@gmail.com <vedantsarawagi123@gmail.com>
2025-04-07 01:26:10 +0530

    changes made in frontend-2

--- frontend/src/components/ApplicationsList.tsx
+++ frontend/src/components/ApplicationsList.tsx
@@ -1,5 +1,5 @@
 // ApplicationsList.tsx
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
--- frontend/src/components/Unauthorized.tsx
+++ frontend/src/components/Unauthorized.tsx
@@ -1,5 +1,5 @@
 // Unauthorized.tsx
-import React from 'react';
+// import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Unauthorized = () => {
